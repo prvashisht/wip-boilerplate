6179988cb4465e7a3a140dc5460357419a213e0a
Pratyush Vashisht <4387947+prvashisht@users.noreply.github.com>
2024-03-11 04:53:13 +0100

    Update websiteDetails structure

--- components/WorkInProgress.vue
+++ components/WorkInProgress.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-col min-h-screen justify-between text-gray-500">
+  <div class="font-mono flex flex-col min-h-screen justify-between text-gray-500">
     <header class="flex flex-col gap-2 md:gap-4 items-center p-4 md:p-8 text-center">
-      <h1 class="font-extrabold text-6xl md:text-8xl text-gray-600 shadowed">{{ websiteName }}</h1>
+      <h1 class="font-extrabold text-6xl md:text-8xl text-gray-600 shadowed">{{ website.name }}</h1>
       <h2 class="font-bold text-xl md:text-3xl text-gray-400">Website under construction</h2>
     </header>
 
@@ -10,33 +10,31 @@
     </main>
 
     <footer class="flex justify-center items-center p-2 text-sm md:text-base text-center bg-gray-100">
-      &copy; {{ new Date().getFullYear() }} {{ websiteName }}
+      &copy; {{ new Date().getFullYear() }} {{ website.name }}
     </footer>
   </div>
 </template>
 
 <script lang="ts" setup>
-const { websites } = useRuntimeConfig().public as { websites: Record<string, string> };
+import { type WebsiteDetails } from '@/types/websiteDetails';
+
+// TODO: Move this to private runtime config
+const { websites }: { websites: Record<string, WebsiteDetails> } = useRuntimeConfig().public;
 const { hostname } = useRequestURL();
-const websiteName = websites[hostname];
+const website = websites[hostname] || websites.default;
 
 useHead({
-  title: `${websiteName} | Website under construction`,
+  title: `${website.name} | Website under construction`,
   meta: [
     {
       name: 'description',
-      content: 'Website under construction'
-    }
-  ]
+      content: website.description || "Website under construction",
+    },
+  ],
 });
 </script>
 
 <style>
-/* TODO: Move to a common place */
-body{
-  font-family: "Montserrat";
-}
-
 .shadowed{
   text-shadow: 
   1px 0px 1px #ccc, 0px 1px 1px #eee, 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,22 @@
-// TODO: Add a default website config
+import { type WebsiteDetails } from "./types/websiteDetails";
+
+const defaultWebsite = {
+  name: "My Website",
+  description: "Website under construction",
+  phone: "",
+  phonelink: "",
+  email: "",
+};
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   runtimeConfig: {
     public: {
-      websites: JSON.parse(process.env.WEBSITES || '{}') as Record<string, string>,
+      websites: {
+        ...JSON.parse(process.env.WEBSITES || '{}'),
+        "default": defaultWebsite,
+      } as Record<string, WebsiteDetails>,
     },
   },
 })
