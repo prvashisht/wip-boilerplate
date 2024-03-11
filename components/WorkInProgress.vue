cc5b89b3e147f8187c5d2680fe657b23fdef6131
Pratyush Vashisht <4387947+prvashisht@users.noreply.github.com>
2024-03-11 06:44:52 +0100

    Refactor footer in WorkInProgress.vue

--- components/WorkInProgress.vue
+++ components/WorkInProgress.vue
@@ -9,9 +9,22 @@
       <BgSvg class="max-w-screen-sm" />
     </main>
 
-    <!-- TODO: Add phone number and other details if available -->
-    <footer class="flex justify-center items-center p-2 text-sm md:text-base text-center bg-gray-100">
-      &copy; {{ new Date().getFullYear() }} {{ website.name }}
+    <footer class="flex flex-col sm:flex-row sm:gap-4 justify-center items-center p-2 text-sm md:text-base text-center bg-gray-100">
+      <div>
+        &copy; {{ new Date().getFullYear() }} {{ website.name }}
+      </div>
+      <div class="hidden sm:block">|</div>
+      <div
+        v-if="website.phone || website.email || website.phonelink"
+      >
+        <span v-if="website.phone || website.phonelink">
+          <a :href=" 'tel:' + (website.phonelink || website.phone)" class="text-blue-400 hover:underline">{{ website.phone || website.phonelink }}</a>
+        </span>
+        <span v-if="(website.phone || website.phonelink) && website.email"> | </span>
+        <span v-if="website.email">
+          <a :href="'mailto:' + website.email" class="text-blue-400 hover:underline">{{ website.email }}</a>
+        </span>
+      </div>
     </footer>
   </div>
 </template>
